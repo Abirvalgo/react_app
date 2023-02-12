6b108a360d55877b6cb5e8dda69377ca6bf977c6
Abirvalgo <113058649+Abirvalgo@users.noreply.github.com>
2023-02-12 13:34:42 +0300

    change prettier dots to tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = () => {
 	return (
@@ -13,6 +13,6 @@ const App = () => {
 			</header>
 		</div>
 	);
-}
+};
 
 export default App;
