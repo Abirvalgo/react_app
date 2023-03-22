6269e430a96378fec8a955c209b0921650369674
Abirvalgo <113058649+Abirvalgo@users.noreply.github.com>
2023-03-22 18:50:17 +0300

    add lesson code(+fix)

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./SignUp.module.scss";
 import Title from "../../components/Title";
@@ -8,12 +8,20 @@ import Button from "../../components/Button";
 import { ButtonType } from "../../utils/@globalTypes";
 import { Theme, useThemeContext } from "../../context/Theme/Context";
 import { RoutesList } from "../Router";
+import { useDispatch } from "react-redux";
+import { signUpUser } from "../../redux/reducers/authSlice";
 
 const SignUp = () => {
 	const [userName, setUserName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+
+	const { theme } = useThemeContext();
+	const isDark = theme === Theme.Dark;
+
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
 	const onChangeUserName = (value: string) => {
 		setUserName(value);
@@ -24,13 +32,70 @@ const SignUp = () => {
 	const onChangePassword = (value: string) => {
 		setPassword(value);
 	};
-	const onSignUpClick = () => {
-		navigate(RoutesList.Confirm);
+	const onChangeConfirmPassword = (value: string) => {
+		setConfirmPassword(value);
 	};
 
-	const { theme } = useThemeContext();
-	const isDark = theme === Theme.Dark;
+	// const onSignUpClick = () => {
+	// 	navigate(RoutesList.Confirm);
+	// };
+	const onSignUpClick = () => {
+		dispatch(
+			signUpUser({
+				data: { username: userName, email, password },
+				callback: () => navigate(RoutesList.SignIn),
+			})
+		);
+	};
 
+	const [nameError, setNameError] = useState("");
+	const [emailError, setEmailError] = useState("");
+	const [passwordError, setPasswordError] = useState("");
+
+	useEffect(() => {
+		if (userName.length === 0) {
+			setNameError("Name is required field");
+		} else {
+			setNameError("");
+		}
+	}, [userName]);
+
+	useEffect(() => {
+		if (email.length === 0) {
+			setEmailError("Email is required field");
+		} else {
+			setEmailError("");
+		}
+	}, [email]);
+
+	useEffect(() => {
+		if (password !== confirmPassword) {
+			setPasswordError("Passwords must match");
+		} else if (password.length === 0 || confirmPassword.length === 0) {
+			setPasswordError("Password is required field");
+		} else {
+			setPasswordError("");
+		}
+	}, [confirmPassword, password]);
+
+	const isValid = useMemo(() => {
+		return (
+			nameError.length === 0 &&
+			emailError.length === 0 &&
+			passwordError.length === 0
+		);
+	}, [nameError, emailError, passwordError]);
+
+	// Используем, если не надо показывать никаких ошибок пользователю
+	// const isValid = useMemo(() => {
+	//   return (
+	//     name.length > 0 &&
+	//     email.length > 0 &&
+	//     password.length > 0 &&
+	//     confirmPassword.length > 0 &&
+	//     password === confirmPassword
+	//   );
+	// }, [name, email, password, confirmPassword]);
 	return (
 		<div
 			className={classNames(styles.container, {
@@ -61,6 +126,7 @@ const SignUp = () => {
 							type={"text"}
 							title="Name"
 							placeholder="Your name"
+							errorText={nameError}
 						/>
 						<Input
 							value={email}
@@ -68,6 +134,7 @@ const SignUp = () => {
 							type={"text"}
 							title="Email"
 							placeholder="Your email"
+							errorText={emailError}
 						/>
 						<Input
 							value={password}
@@ -75,13 +142,15 @@ const SignUp = () => {
 							type={"password"}
 							title="Password"
 							placeholder="Your password"
+							errorText={passwordError}
 						/>
 						<Input
-							value={password}
-							onChange={onChangePassword}
+							value={confirmPassword}
+							onChange={onChangeConfirmPassword}
 							type={"password"}
 							title="Confirm password"
 							placeholder="Confirm password"
+							errorText={passwordError}
 						/>
 					</div>
 					<div className={styles.button}>
@@ -89,6 +158,7 @@ const SignUp = () => {
 							title={"Sign Up"}
 							type={ButtonType.Primary}
 							onClick={onSignUpClick}
+							disabled={!isValid}
 						/>
 					</div>
 					<div
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 
 import themeReducer from "./reducers/themeSlice";
+import authReducer from "./reducers/authSlice";
 import postReducer from "./reducers/postSlice";
 import rootSaga from "./sagas/rootSaga";
 
@@ -11,6 +12,7 @@ const store = configureStore({
 	reducer: {
 		theme: themeReducer,
 		post: postReducer,
+		auth: authReducer,
 	},
 	middleware: [sagaMiddleware],
 });
